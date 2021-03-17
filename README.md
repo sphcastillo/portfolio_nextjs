0dcdcdaa40005515a1d56a16c84536fa561a8ef4
Sophia Castillo <Sphcastillo@gmail.com>
2021-03-17 13:38:48 -0700

    Success page successfully created

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 ## Step 4 : Success
 
 ### Methods: nextStep()
+Increment 
 ### Methods: prevStep()
+Bring to the step before it
 ### Methods: handleChange()
+When we start to enter data into the inputs and it edits the fields of the state 
--- formtemplates/src/components/UserForm.js
+++ formtemplates/src/components/UserForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import FormUserDetails from "./FormUserDetails"
 import FormPersonalDetails from "./FormPersonalDetails"
 import Confirm from "./Confirm";
+import Success from "./Success";
 
 export class UserForm extends Component {
     state = {
@@ -75,7 +76,9 @@ export class UserForm extends Component {
             )
 
             case 4:
-            return <h1>Success</h1>
+            return(
+                <Success />
+            )
         }
     }
 }

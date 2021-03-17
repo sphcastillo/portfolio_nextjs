8269f1a57c5d7e30ba58e95571f0506a46d10258
Sophia Castillo <Sphcastillo@gmail.com>
2021-03-17 12:02:49 -0700

    importing material-ui MuiThemeProvider, AppBar, TextField, RaisedButton

--- formtemplates/src/components/UserForm.js
+++ formtemplates/src/components/UserForm.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import FormUserDetails from "./FormUserDetails"
 
 export class UserForm extends Component {
     state = {

1f6b31af8e366a53800b22cd98eaf4d413655205
Sophia Castillo <Sphcastillo@gmail.com>
2021-03-17 11:46:05 -0700

    created Confirm.js FormPersonalDetails.js Form UserDetails.js Success.js Userform.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# MaterialUI_form_templates
+#UserForm
+
+## Step 1: FormUserDetails
+## Step 2: FormPersonalDetails
+## Step 3: Confirm
+## Step 4 : Success
+
+### Methods: nextStep()
+### Methods: prevStep()
+### Methods: handleChange()
---
+++ formtemplates/src/components/UserForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: "",
+        lastName: "",
+        email: "",
+        occupation: "",
+        city: "",
+        bio: ""
+    }
+
+    // 3 methods
+
+    // Proceed to the next step
+    // take STEP out of the state thru destructing 
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        })
+    }
+
+    // Go back to previous step
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step - 1
+        })
+    }
+
+    // Handle fields change
+    // take whatever the input is and set it to whatever the value is
+    
+    handleChange = input => e => {
+        this.setState({[input] : e.target.value});
+    }
+
+    render() {
+
+        const { step } = this.state;
+        const { firstName, lastName, email, occupation, city, bio } = this.state;
+        const values = { firstName, lastName, email, occupation, city, bio }
+
+        switch(step) {
+            case 1:
+            return(
+                <FormUserDetails 
+                    netStep = {this.nextStep}
+                    handleChange = {this.handleChange}
+                    values={values}
+                />
+            )
+            case 2:
+            return <h1>FormPersonalDetails</h1>
+            
+            case 3:
+            return <h1>Confirm</h1>
+
+            case 4:
+            return <h1>Success</h1>
+        }
+    }
+}
+
+export default UserForm;

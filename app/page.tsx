827e8481e6cd9a556d59141eab8db18a502f3822
sphcastillo <sphcastillo@gmail.com>
2024-07-23 14:15:14 -0700

    update on code / making it a nextjs app

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Header from "@/components/Header";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      {/* <About data={resumeData.main} /> */}
+      {/* <Resume data={resumeData.resume} /> */}
+      {/* <Portfolio data={resumeData.portfolio} /> */}
+      {/* <Testimonials data={resumeData.testimonials} /> */}
+      {/* <Contact data={resumeData.main} /> */}
+      {/* <Footer data={resumeData.main} /> */}
+    </div>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+
+function Header() {
+    // const useTypewriter = (text: String, speed = 50) => {
+    //     const [displayText, setDisplayText] = useState("");
+
+    //     useEffect(() => {
+    //         let i = 0;
+    //         const typingInterval = setInterval(() => {
+    //             if(i < text.length) {
+    //                 setDisplayText(prevText => prevText + text.charAt(i));
+    //                 i++;
+    //             } else {
+    //                 clearInterval(typingInterval);
+    //             }
+    //         }, speed);
+
+    //         return () => {
+    //             clearInterval(typingInterval);
+    //         }
+    //     }, [text, speed]);
+
+    //     return displayText;
+    // }
+
+    // const Typewriter = ({ text, speed }) => {
+    //     const displayText = useTypewriter(text, speed);
+    //     return <span>{displayText}</span>;
+    // }
+
+    // export default Typewriter;
+  return (
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="row banner">
+        <div className="banner-text">
+          <h1 className="responsive-headline">
+            {/* <Typewriter text=""/> */}
+          </h1>
+          <h3>
+            Based in Huntington Beach. <span>Software Engineer</span>. Experienced in NextJS, Typescript, Node, MongoDB ... - I have the tools to make your vision come true. Let's chat.
+          </h3>
+          <hr />
+          {/* <ul className="social">{networks}</ul> */}
+        </div>
+      </div>
+
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
+}
+export default Header;
